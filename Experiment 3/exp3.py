99329a24afb72540a6039a467cfbdf2f1fcd36f6
Survine <hridaybardhan18@gmail.com>
2024-04-25 20:48:51 +0530

    random

---
+++ Experiment 3/exp3.py
@@ -0,0 +1,11 @@
+import random
+num = random.randrange(1,100)
+print(num)
+guess = int(input("Enter your guess from 1 to 100"))
+print(num)
+if guess == num:
+    print("Your guess was correct")
+elif guess + 20 > num:
+    print("Your guess was too high")
+elif guess - 20 < num:
+    print("Your guess was too low")
