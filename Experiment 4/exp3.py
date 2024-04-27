0649adc5e65e257a25ddca316a0ca12ded0d90ff
Survine <hridaybardhan18@gmail.com>
2024-04-27 21:36:09 +0530

    msg

---
+++ Experiment 4/exp3.py
@@ -0,0 +1,14 @@
+score = int(input("Enter your exam score: "))
+
+if 90 <= score <= 100:
+    print("Your grade is A")
+elif 80 <= score < 90:
+    print("Your grade is B")
+elif 70 <= score < 80:
+    print("Your grade is C")
+elif 60 <= score < 70:
+    print("Your grade is D")
+elif 0 <= score < 60:
+    print("Your grade is F")
+else:
+    print("Invalid score entered")
